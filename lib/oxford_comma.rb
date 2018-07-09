9acde719944bde39b8747aac626e2ce6e6feb28f
wachka06 <wachka06@gmail.com>
2018-07-09 22:35:03 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
+  array.join(",")
+end
 
+def oxford_comma(array)
+  if array.size >= 3
+    new_array = array[array.size..array.size -2]
+    return new_array.join(", ") + " and " + array.last
+  else 
+  array.join(" and ")
+ end
 end
