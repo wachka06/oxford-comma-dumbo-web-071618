5407cbd38caa5d96abc2810780d54aa1fdafd992
wachka06 <wachka06@gmail.com>
2018-07-09 22:44:57 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,9 +5,6 @@ end
 def oxford_comma(array)
   if array.size >= 3
     new_array = array[0..(array.size-2)]
-    # new_array2 = []
-    #   new_array.each do |element|
-    #     element + 
     return new_array.join(", ") + ", and " + array.last
   else 
   array.join(" and ")
