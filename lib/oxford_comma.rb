3fce9d2f868a5432ab7d9c660fd45a557f0ea299
wachka06 <wachka06@gmail.com>
2018-07-09 22:45:08 +0000

    done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
+  array.join(",")
+end
 
+def oxford_comma(array)
+  if array.size >= 3
+    new_array = array[0..(array.size-2)]
+    return new_array.join(", ") + ", and " + array.last
+  else 
+  array.join(" and ")
+ end
 end
